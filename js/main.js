1278fbbeea6e5fadd02b5a9ef2ad1587aecb6a96
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-03 17:12:37 +0100

    done hard but problme cause generate many squares

--- js/main.js
+++ js/main.js
@@ -1,60 +1,44 @@
 const containerDom = document.getElementById("grid_container");
 const gameModeDom = document.getElementById("game_mode");
-// const easyDom = document.querySelector(".easy");
-// const hardDom = document.querySelector(".hard");
-// if (gameModeDom.value == easyDom){
-//     for (let i = 0; i < 100 ; i++){
-//         const squareDom = document.createElement('div');
-//         squareDom.innerHtml = i;
-//         squareDom.classList.add('square');
-         
-//         squareDom.addEventListener('click',
-//             function(){
-//                 this.classList.toggle('selected');
-//                 console.log(i + 1);
-//             }       
-//         );
-    
-//         const buttonDom = document.getElementById("action");
-        
-//         buttonDom.addEventListener('click',
-//             function(){
-//                 squareDom.classList.toggle('d_none');
-//                 squareDom.classList.remove('selected');
-//             }  
-//         );
-//         containerDom.append(squareDom);
-//     };
-//     } else {(gameModeDom.value == hardDom)
-//     for (let i = 0; i < 81 ; i++){
-//         const squareDom = document.createElement('div');
-//         squareDom.innerHtml = i;
-//         squareDom.classList.add('square');
-         
-//         squareDom.addEventListener('click',
-//             function(){
-//                 this.classList.toggle('selected');
-//                 console.log(i + 1);
-//             }       
-//         );
-    
-//         const buttonDom = document.getElementById("action");
-        
-//         buttonDom.addEventListener('click',
-//             function(){
-//                 squareDom.classList.toggle('d_none');
-//                 squareDom.classList.remove('selected');
-//             }  
-//         );
-//         containerDom.append(squareDom);
-//     };
-//            };
+const buttonDom = document.getElementById("action");
 
-           for (let i = 0; i < 100 ; i++){
+        //    for (let i = 0; i < 100 ; i++){
+        //     const squareDom = document.createElement('div');
+        //     squareDom.innerHtml = i;
+        //     squareDom.classList.add('square');
+        //     squareDom.classList.add('d_none');
+        //     squareDom.addEventListener('click',
+        //         function(){
+        //             this.classList.toggle('selected');
+        //             console.log(i + 1);
+        //         }       
+        //     );                  
+            
+        //     buttonDom.addEventListener('click',
+        //         function(){
+        //             squareDom.classList.toggle('d_none');
+        //             squareDom.classList.remove('selected');
+        //         }  
+        //     );
+        //     containerDom.append(squareDom);
+        // };
+
+
+
+
+
+
+
+buttonDom.addEventListener('click',
+function(){
+    if (gameModeDom.value == "easy"){
+        for (let i = 0; i < 100 ; i++){
             const squareDom = document.createElement('div');
             squareDom.innerHtml = i;
             squareDom.classList.add('square');
             squareDom.classList.add('d_none');
+
+            
             squareDom.addEventListener('click',
                 function(){
                     this.classList.toggle('selected');
@@ -62,7 +46,7 @@ const gameModeDom = document.getElementById("game_mode");
                 }       
             );
         
-            const buttonDom = document.getElementById("action");
+        
             
             buttonDom.addEventListener('click',
                 function(){
@@ -72,10 +56,44 @@ const gameModeDom = document.getElementById("game_mode");
             );
             containerDom.append(squareDom);
         };
+        } else {(gameModeDom.value == "hard")
+        for (let i = 0; i < 81 ; i++){
+            const squareDom = document.createElement('div');
+            squareDom.innerHtml = i;
+            squareDom.classList.add('square');
+            squareDom.classList.add('d_none');
+            
+            squareDom.addEventListener('click',
+                function(){
+                    this.classList.toggle('selected');
+                    console.log(i + 1);
+                }       
+            );
+        
+            
+            
+            buttonDom.addEventListener('click',
+                function(){
+                    squareDom.classList.toggle('d_none');
+                    squareDom.classList.remove('selected');
+                }  
+            );
+            containerDom.append(squareDom);
+        };
+            };
+
+}
+   
 
 
-// mettere in maniera progressiva numeri nei quadrati
-// creare un bottone per generare la griglia
-// al click cambiare colore dei quadrati
+
+);
+
+
+
+
+
+
+
 
 
