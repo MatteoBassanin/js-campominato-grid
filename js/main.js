24eef6cc889725e4b5a08ae07bfedf6c30f1f977
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-03 15:22:40 +0100

    toogle colored squares

--- js/main.js
+++ js/main.js
@@ -4,7 +4,22 @@ const containerDom = document.getElementById("grid_container");
 for (let i = 0; i < 100 ; i++){
     const squareDom = document.createElement('div');
     squareDom.classList.add('square');
+     
+    squareDom.addEventListener('click',
+        function(){
 
+            this.classList.toggle('selected');
+        }
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    );
 
 
 
@@ -12,3 +27,13 @@ for (let i = 0; i < 100 ; i++){
 
     containerDom.append(squareDom);
 }
+
+
+
+
+
+// mettere in maniera progressiva numeri nei quadrati
+// creare un bottone per generare la griglia
+// al click cambiare colore dei quadrati
+
+
