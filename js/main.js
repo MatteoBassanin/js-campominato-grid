0a93167a8e43e6b109afd0e5b3b0ac25d9c944d3
MatteoBassanin <matteo.bassanin@gmail.com>
2023-03-04 17:15:17 +0100

    fixing squares and making proper function

--- js/main.js
+++ js/main.js
@@ -2,16 +2,22 @@ const containerDom = document.getElementById("grid_container");
 const gameModeDom = document.getElementById("game_mode");
 const buttonDom = document.getElementById("action");
 
-      
+     
 
 
 
-function easyGame(){
-    for (let i = 0; i < 100 ; i++){
+function setDiffcult(mode){
+    containerDom.innerHTML = "";
+    let totalSquare;
+    if (mode == "easy"){totalSquare = 100};
+    if (mode == "hard"){totalSquare = 81};
+    if (mode == "mamma_mia"){totalSquare = 49};
+
+    for (let i = 0; i < totalSquare ; i++){
             const squareDom = document.createElement('div');
             squareDom.innerHtml = i;
-            squareDom.classList.add('square');
-            squareDom.classList.add('d_none');
+            squareDom.classList.add('square_' + mode);
+
             containerDom.append(squareDom);
             squareDom.addEventListener('click',
             
@@ -27,76 +33,20 @@ function easyGame(){
                     squareDom.classList.remove('selected');
                 }  
             );
-            containerDom.append(squareDom);
+        
         };
 }
 
-function hardGame(){
-    for (let i = 0; i < 81 ; i++){
-        const squareDom = document.createElement('div');
-        squareDom.innerHtml = i;
-        // squareDom.classList.remove('square');
-        squareDom.classList.add('square_hard');
-        squareDom.classList.add('d_none');
 
-        containerDom.append(squareDom);
-        squareDom.addEventListener('click',
-        
-            function(){
-                this.classList.toggle('selected');
-                console.log(i + 1);
-            }       
-        );                  
-        
-        buttonDom.addEventListener('click',
-            function(){
-                squareDom.classList.toggle('d_none');
-                squareDom.classList.remove('selected');
-            }  
-        );
-        containerDom.append(squareDom);
-    };
-}
-
-function mammaMia(){
-    for (let i = 0; i < 49 ; i++){
-        const squareDom = document.createElement('div');
-        squareDom.innerHtml = i;
-        squareDom.classList.add('square');
-        squareDom.classList.add('d_none');
-        containerDom.append(squareDom);
-        squareDom.addEventListener('click',
-        
-            function(){
-                this.classList.toggle('selected');
-                console.log(i + 1);
-            }       
-        );                  
-        
-        buttonDom.addEventListener('click',
-            function(){
-                squareDom.classList.toggle('d_none');
-                squareDom.classList.remove('selected');
-            }  
-        );
-        containerDom.append(squareDom);
-    };
-}
-
+ 
 
 buttonDom.addEventListener('click',
     function(){
-        if (gameModeDom.value == "easy"){
-            easyGame();
-        }else if (gameModeDom.value == "hard"){
-            hardGame();
-        }else if (gameModeDom.value == "mamma_mia"){
-            mammaMia();
+        
+            setDiffcult(gameModeDom.value);
+            
+    });
 
-        }
-    }
-
-);
 
 
 
